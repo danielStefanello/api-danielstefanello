62f85bfce67e7e465f0a5df61aadb718d682a5b1
Daniel Stefanello <danieldfst@hotmail.com>
2020-02-27 10:17:57 -0300

    First commit - Create and uddate users.

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'danielstefanello',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionControler';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+export default routes;
